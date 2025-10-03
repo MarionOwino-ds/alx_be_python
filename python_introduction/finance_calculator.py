4aaef199f55930ecf1ca3040caf5fd3375ea0bbe
MarionOwino-ds <marionangelaowino@gmail.com>
2025-10-04 00:33:00 +0300

    projected_savings

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,9 @@
+monthly_income = int (input ("Enter your monthly income: "))
+total_monthly_expenses = int (input ("Enter your total monthly expenses: "))    
+
+monthly_savings = monthly_income - total_monthly_expenses
+
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+
+print(monthly_savings)
+print(projected_savings)
