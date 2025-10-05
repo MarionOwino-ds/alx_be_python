4501af6d327cad527264364e32ea2520b888eac4
MarionOwino-ds <marionangelaowino@gmail.com>
2025-10-05 16:37:09 +0300

    calculator corrections

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -2,25 +2,31 @@ import unittest
 from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
+
     def setUp(self):
+        """Set up the calculator before each test."""
         self.calc = SimpleCalculator()
 
     def test_addition(self):
+        """Test addition method."""
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(0, 0), 0)
 
     def test_subtraction(self):
+        """Test subtraction method."""
         self.assertEqual(self.calc.subtract(10, 5), 5)
         self.assertEqual(self.calc.subtract(0, 5), -5)
         self.assertEqual(self.calc.subtract(-5, -5), 0)
 
     def test_multiply(self):
+        """Test multiply method."""
         self.assertEqual(self.calc.multiply(3, 4), 12)
         self.assertEqual(self.calc.multiply(-2, 5), -10)
         self.assertEqual(self.calc.multiply(0, 10), 0)
 
     def test_divide(self):
+        """Test divide method."""
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(-9, 3), -3)
         self.assertIsNone(self.calc.divide(5, 0))
